3a0c33eaef32d703c55fad4ef4c142175763a32f
Rajkumar17 <rajkumarm1703@gmail.com>
2024-12-04 19:44:58 +0530

    favorite number

---
+++ fav_num.py
@@ -0,0 +1,8 @@
+# 8 find favorite number
+n = int(input("enter a number : ")) 
+if n == 1 or n == 2 or n == 3 or n ==10:
+    print("favorite number")
+elif n == 4 or n == 5 or n == 7 or n == 9:
+    print("not a favorite number")
+else :
+    print("please enter valid number")
